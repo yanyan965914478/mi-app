e35eea404d3d52f52e7c8cd33cda0111347251a5
宋 <965914478@qq.com>
2019-08-27 18:49:32 +0800

    swiper

---
+++ src/components/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <mt-swipe :auto="3000" :continuous="true">
+      <mt-swipe-item v-for="(item,index) in swiperArr" :key="index">
+        <img :src="item.img" alt />
+      </mt-swipe-item>
+    </mt-swipe>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      swiperArr: [
+        {
+          id: 1,
+          img:
+            'http://cdn.cnbj1.fds.api.mi-img.com/mi-mall/939a2f677670d5c852b07ce574a78cae.jpg?thumb=1&w=720&h=360'
+        },
+        {
+          id: 2,
+          img:
+            'http://cdn.cnbj1.fds.api.mi-img.com/mi-mall/5d40feacc4d4fc31b3e6e65aa9891ea5.jpg?thumb=1&w=720&h=360'
+        },
+        {
+          id: 3,
+          img:
+            'http://cdn.cnbj1.fds.api.mi-img.com/mi-mall/63e0e2bf0e7cae90dd848823852a7247.jpg?thumb=1&w=720&h=360'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style scoped>
+div {
+  width: 100%;
+  height: 320px;
+  background: pink;
+}
+div img {
+  width: 100%;
+  height: 320px;
+}
+</style>
--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>首页</div>
+    <!-- 轮播图 -->
+   <swiper></swiper>
 </template>
+<script>
+import Swiper from './components/Swiper'
+export default {
+  components: {
+    Swiper
+  }
+}
+</script>
+<style scoped>
+
+</style>
